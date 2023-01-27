83220f521672e69eb1c4b52729ea591af5652f74
Kimjimin97 <sja9707@daum.net>
2023-01-27 19:09:53 +0900

    헤더 색상 추가

--- frontend/src/pages/component/header.js
+++ frontend/src/pages/component/header.js
@@ -7,7 +7,7 @@ function Header() {
     <header class="bg-[#ECE4D7]">
       <nav>
         {/* 맨 위 헤더 */}
-        <div class="flex place-items-center">
+        <div class="flex justify-between place-items-center mx-30">
           <img class="w-10 h-10" src="/instagram.png" />
           <div>Greeny Day</div>
           <ul class="flex font-[300]">
@@ -21,7 +21,7 @@ function Header() {
         </div>
         {/* 두 번째 헤더 */}
         <div>
-          <ul class="flex place-items-center">
+          <ul class="flex justify-between  place-items-center">
             <li class="">
               <a href="/loginpage/login">About</a>
             </li>
