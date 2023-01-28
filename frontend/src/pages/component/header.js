e8a15b11738f21967ac3e869cc7956067a5ad153
Kimjimin97 <sja9707@daum.net>
2023-01-28 23:28:22 +0900

    이미지 url 추가

--- frontend/src/pages/component/header.js
+++ frontend/src/pages/component/header.js
@@ -2,14 +2,26 @@ import { Menu } from "antd";
 import LOGIN from "../loginpage/login";
 import SIGNUP from "../loginpage/signup";
 
+import axios from "axios";
+import { backUrl } from "../../config/config";
+import { useEffect } from "react";
+
 function Header() {
+  var imgurl = "";
+  useEffect(() => {
+    axios.get(backUrl + "/items/").then((res) => {
+      imgurl = backUrl + res.data.items[0].image[0];
+      console.log(typeof imgurl);
+    });
+  });
   return (
     <header class="bg-[#ECE4D7]">
       <nav>
         {/* 맨 위 헤더 */}
         <div class="flex justify-between place-items-center mx-30">
-          <img class="w-10 h-10" src="/instagram.png" />
-          <div>Greeny Day</div>
+          {/* <img class="w-10 h-10" src="/instagram.png" /> */}
+          <img class="w-10 h-10" src={""} />
+
           <ul class="flex font-[300]">
             <li class="mr-3">
               <a href="/loginpage/login">Login</a>
