03acbf6297bb9fde01019b65eb1718ddb9b31107
Kimjimin97 <sja9707@daum.net>
2023-02-03 23:44:02 +0900

    gitignore

---
+++ frontend/src/pages/component/menu.js
@@ -0,0 +1,9 @@
+import Header from "./component/header";
+import { Carousel } from "@mantine/carousel";
+import { Center, AspectRatio, Image, MediaQuery, Space } from "@mantine/core";
+
+function Menu() {
+  return <div class="bg-[#ECE4D7]"></div>;
+}
+
+export default Menu;
