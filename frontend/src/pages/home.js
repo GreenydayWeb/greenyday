edc0f6f41b4f460854f4b3706ae544ec252f8dc9
Kimjimin97 <sja9707@daum.net>
2023-02-05 00:02:49 +0900

    캐로셀 이미지 추가

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,43 +2,32 @@ import Header from "./component/header";
 import { Carousel } from "@mantine/carousel";
 import { Center, AspectRatio, Image, MediaQuery, Space } from "@mantine/core";
 
+const images = ["/event4.jpg", "/event4.jpg", "/event5.jpg"];
+
 function Home() {
+  const slides = images.map((url) => (
+    <Carousel.Slide key={url}>
+      <Image src={url} />
+    </Carousel.Slide>
+  ));
   return (
     <div class="bg-[#ECE4D7]">
       <div>
         <Header />
-        <div class="mt-10 w-1000 h-1000">
-          <Center>
-            <Carousel
-              slideSize="50%"
-              withIndicators={true}
-              controlSize={72}
-              breakpoints={[
-                { maxWidth: 1200, height: 678 },
-                { maxWidth: 1013, height: 571 },
-                { maxWidth: 768, height: 253 },
-              ]}
-            >
-              <Carousel.Slide>
-                {/* <AspectRatio ratio={16 / 9}> */}
-                <Image fit="contain" src="/event3.jpg" />
-                {/* </AspectRatio> */}
-              </Carousel.Slide>
-              <Carousel.Slide>
-                {" "}
-                {/* <Image fit="contain" src="/event3.jpg" /> */}
-              </Carousel.Slide>
-
-              <Carousel.Slide>
-                <Image fit="contain" src="/event3.jpg" />
-              </Carousel.Slide>
-              {/* ...other slides */}
-            </Carousel>{" "}
-          </Center>
+        <div class="mt-10 ">
+          <Carousel sx={{ maxWidth: 1200 }} mx="auto" withIndicators={true}>
+            {slides}
+          </Carousel>
         </div>
       </div>
     </div>
   );
 }
+
+// breakpoints={[
+//   { maxWidth: 1200, height: 678 },
+//   { maxWidth: 1013, height: 571 },
+//   { maxWidth: 768, height: 253 },
+// ]}
 
 export default Home;
