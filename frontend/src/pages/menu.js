423129898b621b3197c19ed5abcb0841a43b5cc5
Kimjimin97 <sja9707@daum.net>
2023-02-09 14:21:28 +0900

    메뉴 헤더 추가

---
+++ frontend/src/pages/menu.js
@@ -0,0 +1,13 @@
+import MenuHeader from "./component/menu_header";
+import Footer from "./component/footer";
+
+function Menu() {
+  return (
+    <div class="bg-[#ECE4D7]">
+      <MenuHeader />
+      <Footer />
+    </div>
+  );
+}
+
+export default Menu;
