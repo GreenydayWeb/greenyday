e129030d0a774b8cb936594023cf30076b1f7eb0
Kimjimin97 <sja9707@daum.net>
2023-02-13 23:12:53 +0900

    회원가입 모달 없애기

--- frontend/src/pages/loginpage/signup.js
+++ frontend/src/pages/loginpage/signup.js
@@ -82,282 +82,259 @@ const Signup = () => {
 
   return (
     <>
-      <div
-        type="primary"
-        onClick={showModal}
+      <Text
         style={{
-          fontSize: "32px",
-          fontWeight: "400",
-          color: "rgba(0, 0, 0, 1)",
+          fontSize: 44,
+          fontWeight: 700,
           fontFamily: "sansneo_light",
+          color: "rgba(96, 96, 96, 1)",
         }}
       >
         회원가입
-      </div>
-      <Modal
-        title={
-          <Text
-            style={{
-              fontSize: 44,
-              fontWeight: 700,
-              fontFamily: "sansneo_light",
-              color: "rgba(96, 96, 96, 1)",
-            }}
-          >
-            회원가입
-          </Text>
-        }
-        footer={null}
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
+      </Text>
+
+      <div
+        style={{
+          fontSize: 16,
+          fontWeight: 500,
+          color: "rgba(121, 120, 120, 1)",
+          fontFamily: "sansneo_light",
+          margin: "0 20px 20px 0",
+        }}
       >
-        <div
-          style={{
-            fontSize: 16,
-            fontWeight: 500,
-            color: "rgba(121, 120, 120, 1)",
-            fontFamily: "sansneo_light",
-            margin: "0 20px 20px 0",
-          }}
-        >
-          샐러드를 주문하려면 회원가입이 필요해요!{" "}
-        </div>
-        <Form
-          name="basic"
-          initialValues={{
-            remember: true,
-          }}
-          layout="vertical"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          {/* 이메일 */}
-          <div>
-            {" "}
-            {/* 이메일이 중복되지 않고, 이전 이메일과 달라진 겨우 */}
-            {!emailOverLap ? (
+        샐러드를 주문하려면 회원가입이 필요해요!{" "}
+      </div>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        {/* 이메일 */}
+        <div>
+          {" "}
+          {/* 이메일이 중복되지 않고, 이전 이메일과 달라진 겨우 */}
+          {!emailOverLap ? (
+            <Form.Item
+              label={<Text style={fontStyle}>이메일</Text>}
+              name="email"
+              rules={[
+                {
+                  type: "email",
+                  message: "올바른 이메일을 입력해 주세요.",
+                },
+
+                {
+                  required: true,
+                  message: "이메일을 입력해 주세요!",
+                },
+              ]}
+            >
+              <Input
+                style={{
+                  borderRadius: "19px",
+                  background: "rgba(217, 217, 217, 1)",
+                }}
+                placeholder="예) greenyday1234@gmail.com"
+              />
+            </Form.Item>
+          ) : (
+            <div>
               <Form.Item
                 label={<Text style={fontStyle}>이메일</Text>}
                 name="email"
-                rules={[
-                  {
-                    type: "email",
-                    message: "올바른 이메일을 입력해 주세요.",
-                  },
-
-                  {
-                    required: true,
-                    message: "이메일을 입력해 주세요!",
-                  },
-                ]}
+                validateStatus="error"
+                help="중복된 이메일입니다."
               >
                 <Input
-                  style={{
-                    borderRadius: "19px",
-                    background: "rgba(217, 217, 217, 1)",
-                  }}
+                  style={{ borderRadius: "0px" }}
                   placeholder="예) greenyday1234@gmail.com"
+                  onChange={emailOnChange}
                 />
               </Form.Item>
-            ) : (
-              <div>
+            </div>
+          )}{" "}
+        </div>
+        <Form.Item
+          label={<Text style={fontStyle}>비밀번호</Text>}
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "비밀번호를 입력해 주세요!",
+            },
+          ]}
+        >
+          <Input.Password
+            style={{ borderRadius: "0px" }}
+            placeholder="영문,숫자 조합 8-16자"
+          />
+        </Form.Item>
+        <Form.Item
+          name="confirm"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "비밀번호를 한번 더 입력해주세요!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("비밀번호가 일치하지 않습니다.")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password
+            style={{ borderRadius: "0px" }}
+            placeholder="비밀번호를 한번 더 입력해 주세요!"
+          />
+        </Form.Item>
+        <Row justify="space-between">
+          <Col span={11}>
+            {/* 이름 입력 */}
+            <Form.Item
+              name="name"
+              label={<Text style={fontStyle}>이름</Text>}
+              rules={[
+                {
+                  required: true,
+                  message: "이름을 입력해 주세요!",
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input placeholder="예) 홍길동" style={{ borderRadius: "0px" }} />
+            </Form.Item>
+          </Col>
+          <Col span={11}>
+            {/* 닉네임 입력 */}
+
+            <div>
+              {!nicknameOverLap ? (
                 <Form.Item
-                  label={<Text style={fontStyle}>이메일</Text>}
-                  name="email"
-                  validateStatus="error"
-                  help="중복된 이메일입니다."
+                  name="nickname"
+                  label={<Text style={fontStyle}>닉네임</Text>}
+                  rules={[
+                    {
+                      required: true,
+                      message: "닉네임을 입력해 주세요!",
+                      whitespace: true,
+                    },
+                  ]}
                 >
                   <Input
+                    placeholder="예) 홍길동"
                     style={{ borderRadius: "0px" }}
-                    placeholder="예) greenyday1234@gmail.com"
-                    onChange={emailOnChange}
                   />
                 </Form.Item>
-              </div>
-            )}{" "}
-          </div>
-          <Form.Item
-            label={<Text style={fontStyle}>비밀번호</Text>}
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "비밀번호를 입력해 주세요!",
-              },
-            ]}
-          >
-            <Input.Password
-              style={{ borderRadius: "0px" }}
-              placeholder="영문,숫자 조합 8-16자"
-            />
-          </Form.Item>
-          <Form.Item
-            name="confirm"
-            dependencies={["password"]}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: "비밀번호를 한번 더 입력해주세요!",
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue("password") === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error("비밀번호가 일치하지 않습니다.")
-                  );
+              ) : (
+                <Form.Item
+                  name="nickname"
+                  label={<Text style={fontStyle}>닉네임</Text>}
+                  validateStatus="error"
+                  help="중복된 닉네임입니다."
+                >
+                  <Input
+                    placeholder="예) 홍길동"
+                    style={{ borderRadius: "0px" }}
+                    onChange={nicknameOnChange}
+                  />
+                </Form.Item>
+              )}
+            </div>
+          </Col>
+        </Row>
+        <Row justify="space-between">
+          <Col span={11}>
+            {/* 생년월일 입력 */}
+            <Form.Item
+              name="datepicker"
+              label={<Text style={fontStyle}>생년월일</Text>}
+              rules={[
+                {
+                  type: "object",
+                  required: true,
+                  message: "생년월일을 입력해 주세요!",
                 },
-              }),
-            ]}
-          >
-            <Input.Password
-              style={{ borderRadius: "0px" }}
-              placeholder="비밀번호를 한번 더 입력해 주세요!"
-            />
-          </Form.Item>
-          <Row justify="space-between">
-            <Col span={11}>
-              {/* 이름 입력 */}
-              <Form.Item
-                name="name"
-                label={<Text style={fontStyle}>이름</Text>}
-                rules={[
-                  {
-                    required: true,
-                    message: "이름을 입력해 주세요!",
-                    whitespace: true,
-                  },
-                ]}
-              >
-                <Input
-                  placeholder="예) 홍길동"
-                  style={{ borderRadius: "0px" }}
-                />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              {/* 닉네임 입력 */}
+              ]}
+            >
+              <DatePicker
+                style={{ borderRadius: "0px", width: "100%" }}
+                placeholder="0000-00-00"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={11}>
+            <Form.Item
+              name="phonenumber"
+              label={<Text style={fontStyle}>휴대폰 번호</Text>}
+              rules={[
+                {
+                  required: true,
+                  message: "전화번호를 입력해 주세요!",
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input
+                placeholder="000-000-000"
+                style={{ borderRadius: "0px" }}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Divider />
+        <Row gutter={[3, 20]}>
+          <Col span={24}>
+            <Row gutter={[10, 10]}>
+              <Col span={1}>
+                <Image src="/circle.png"></Image>
+              </Col>
 
-              <div>
-                {!nicknameOverLap ? (
-                  <Form.Item
-                    name="nickname"
-                    label={<Text style={fontStyle}>닉네임</Text>}
-                    rules={[
-                      {
-                        required: true,
-                        message: "닉네임을 입력해 주세요!",
-                        whitespace: true,
-                      },
-                    ]}
-                  >
-                    <Input
-                      placeholder="예) 홍길동"
-                      style={{ borderRadius: "0px" }}
-                    />
-                  </Form.Item>
-                ) : (
-                  <Form.Item
-                    name="nickname"
-                    label={<Text style={fontStyle}>닉네임</Text>}
-                    validateStatus="error"
-                    help="중복된 닉네임입니다."
-                  >
-                    <Input
-                      placeholder="예) 홍길동"
-                      style={{ borderRadius: "0px" }}
-                      onChange={nicknameOnChange}
-                    />
-                  </Form.Item>
-                )}
+              <div fontSize="14px">
+                “가입하기”를 클릭하면 저희 서비스 이용약관 가입에 동의하게
+                됩니다.
+                <br />
+                이메일과 수신정보는 마이페이지에서 언제든 옵트 아웃하실 수
+                있어요.{" "}
               </div>
-            </Col>
-          </Row>
-          <Row justify="space-between">
-            <Col span={11}>
-              {/* 생년월일 입력 */}
-              <Form.Item
-                name="datepicker"
-                label={<Text style={fontStyle}>생년월일</Text>}
-                rules={[
-                  {
-                    type: "object",
-                    required: true,
-                    message: "생년월일을 입력해 주세요!",
-                  },
-                ]}
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{
+                  width: "100%",
+                  height: "57px",
+                  backgroundColor: "rgba(0, 219, 61, 1)",
+                }}
               >
-                <DatePicker
-                  style={{ borderRadius: "0px", width: "100%" }}
-                  placeholder="0000-00-00"
-                />
-              </Form.Item>
-            </Col>
-            <Col span={11}>
-              <Form.Item
-                name="phonenumber"
-                label={<Text style={fontStyle}>휴대폰 번호</Text>}
-                rules={[
-                  {
-                    required: true,
-                    message: "전화번호를 입력해 주세요!",
-                    whitespace: true,
-                  },
-                ]}
-              >
-                <Input
-                  placeholder="000-000-000"
-                  style={{ borderRadius: "0px" }}
-                />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Divider />
-          <Row gutter={[3, 20]}>
-            <Col span={24}>
-              <Row gutter={[10, 10]}>
-                <Col span={1}>
-                  <Image src="/circle.png"></Image>
-                </Col>
-
-                <div fontSize="14px">
-                  “가입하기”를 클릭하면 저희 서비스 이용약관 가입에 동의하게
-                  됩니다.
-                  <br />
-                  이메일과 수신정보는 마이페이지에서 언제든 옵트 아웃하실 수
-                  있어요.{" "}
-                </div>
-              </Row>
-            </Col>
-            <Col span={24}>
-              <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
+                <Text
                   style={{
-                    width: "100%",
-                    height: "57px",
-                    backgroundColor: "rgba(0, 219, 61, 1)",
+                    color: "rgba(255, 255, 255, 1)",
+                    fontSize: "24px",
                   }}
                 >
-                  <Text
-                    style={{
-                      color: "rgba(255, 255, 255, 1)",
-                      fontSize: "24px",
-                    }}
-                  >
-                    가입하기
-                  </Text>
-                </Button>
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
-      </Modal>
+                  가입하기
+                </Text>
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 };
