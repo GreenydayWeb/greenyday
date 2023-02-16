19d07cf0399de4edd6fa939a5cd8334656eb3a3c
Kimjimin97 <sja9707@daum.net>
2023-02-16 20:51:21 +0900

    오늘의 메뉴 텍스트 overflow 해결

--- frontend/src/pages/component/menuto.js
+++ frontend/src/pages/component/menuto.js
@@ -7,7 +7,10 @@ const MenuTo = ({ menu }) => {
     <div class="flex flex-col place-items-center  max-w-[260px] h-[400px] rounded-[80px] shadow bg-[#CEC0AC] ">
       <img class="object-scale-down " src={imgurl}></img>
       <div class="text-[#FEF5E9] text-[25px] mb-[10px]">{menu.name}</div>
-      <div>{menu.description}</div>
+
+      <p class=" mx-3  text-center overflow-ellipsis overflow-hidden">
+        {menu.description}
+      </p>
     </div>
   );
 };
