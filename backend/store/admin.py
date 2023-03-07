71b4e7e7416481702ecd0c9c9a1be9cb7772e1a1
donghyeok1 <07385@naver.com>
2023-03-07 15:53:40 +0900

    주문 관련 모델 생성

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from store.models import Franchise, Cart, Order, Item, Payment, Franchise_Img
+
+
+class ImageInline(admin.TabularInline):
+    model = Franchise_Img
+    fk_name = 'franchise_id'
+    can_delete = False
+@admin.register(Franchise)
+class FranchiseAdmin(admin.ModelAdmin):
+    inlines = (ImageInline,)
+    list_display = ['pk', 'name']
+    list_display_links = ['name']
+
+    def get_franchise_img(self, obj):
+        url = obj.franchise_img.image.url if obj.franchise_img else None
+        return mark_safe(f"<img src={url} style='width: 100px;' />")
+
+    get_franchise_img.short_description = '이미지'
--- backend/store/models.py
+++ backend/store/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+from greenyday.models import Item
 
 # Create your models here.
 
+User = get_user_model()
 class TimestampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -10,7 +14,6 @@ class TimestampedModel(models.Model):
         abstract = True
 
 class Franchise(TimestampedModel):
-    franchise_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.TextField()
     phone_number = models.CharField(max_length=20)
@@ -18,3 +21,35 @@ class Franchise(TimestampedModel):
 
     def __str__(self):
         return self.name
+
+class Franchise_Img(TimestampedModel):
+    franchise_id = models.OneToOneField(Franchise, related_name='franchiseimges', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, unique=True)
+    photo = models.ImageField(upload_to="greenyday/franchise/%Y/%m/%d")
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    payment_status = models.BooleanField(default=False)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user} - {self.order_date}"
+
+
+class Payment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=50)
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.order} - {self.payment_method} - {self.payment_amount}"
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.user} - {self.product} - {self.quantity}"
