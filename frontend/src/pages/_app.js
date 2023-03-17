5647560cf11d2490ad3022eb630fedbf48c79602
Kimjimin97 <sja9707@daum.net>
2023-03-17 22:08:56 +0900

    메타태그 설정

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -16,8 +16,13 @@ function App({ Component, pageProps }) {
           name="description"
           content="당신의 하루에 건강함을 채워 줄 샐러드 전문 브랜드 GREENDAY 입니다."
         />
+        <meta property="og:title" content="GreenyDay" />
+        <meta
+          property="og:description"
+          content="당신의 하루에 건강함을 채워 줄 샐러드 전문 브랜드 GREENDAY 입니다."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image" content="/mainimg.jpg" />
+        <meta property="og:image" content="/logo_grd.png" />
         <link rel="icon" href="/logo_grd.png" />
       </Helmet>
       <Component {...pageProps} />
