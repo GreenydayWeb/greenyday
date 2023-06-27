5f57a803641fbd170a84f2cdbb7a3b819d6b2def
donghyeok1 <07385@naver.com>
2023-06-27 23:53:13 +0900

    feat:user ddd아키텍처 적용

---
+++ backend/urls.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include("greenyday.urls")),
+    path('api/accounts/', include("accounts.urls")),
+    path('api/v1/users', include("backend.users.api.v1")),
+    path('api/v1/items', include("backend.items.api.v1")),
+    path('api/v1/events', include("backend.events.api.v1")),
+]
+
+
+if settings.DEBUG:
+
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
